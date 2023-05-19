026160cc32778d504cbc8a88b2bd5f885ec3d420
William M. Tsikata <tsikatawill@gmail.com>
2023-05-19 12:53:17 +0000

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Container } from "./components/Container";
+
+function App() {
+  return (
+    <>
+      <Container>
+        <h1 className="text-blue-500 mb-5">App</h1>
+      </Container>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Container.jsx
@@ -0,0 +1,7 @@
+export const Container = ({ children }) => {
+  return (
+    <div className="max-w-screen-xl mx-auto px-5 md:px-8 lg:px-0">
+      {children}
+    </div>
+  );
+};
